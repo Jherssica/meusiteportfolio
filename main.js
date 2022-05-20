fef3d8de7a42e6ad83fff0e4a6c389e051186574
Jherssica <jherssyca@gmail.com>
2022-05-19 23:52:25 -0300

    resultado

---
+++ main.js
@@ -0,0 +1,44 @@
+const menuBtn=document.querySelector('#menu-btn');
+const closeBtn=document.querySelector('#close-btn');
+const menu=document.querySelector('nav .container ul');
+
+//abrir menu
+menuBtn.addEventListener('click',() => {
+    menu.style.display = 'block';
+    menuBtn.style.display='none';
+    closeBtn.style.display='inline-block';
+
+    
+})
+
+//fechar menu
+closeBtn.addEventListener('click',() => {
+    menu.style.display = 'none';
+    menuBtn.style.display='inline-block';
+    closeBtn.style.display='none';
+
+    
+})
+
+
+
+
+const navItems=menu.querySelectorAll('li');
+
+
+const changeActiveItem =() =>{
+    navItems.forEach(item =>  {
+        const link=item.querySelector('a');
+        link.classList.remove('active');  
+    })
+}
+
+
+navItems.forEach(item =>  {
+    const link=item.querySelector('a');
+    link.addEventListener('click',() => {
+        changeActiveItem();
+        link.classList.add('active');
+    })
+})
+
